857b420c7d9a15d465bb63bfc8616a8595a11a00
Cl3arglass <haltaffer@gmail.com>
2021-09-28 18:31:50 -0700

    fix next config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,3 @@
 module.exports = {
-  async rewrites() {
-    afterFiles: [
-      // These rewrites are checked after headers/redirects
-      // and before all files including _next/public files which
-      // allows overriding page files
-      {
-        source: '/',
-        destination: '/about',
-        has: [{ type: 'query', key }],
-      },
-    ]
-  },
+  
 }
