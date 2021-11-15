02ff20f4806d89df18038b1601ff77292d07ac16
Cl3arglass <haltaffer@gmail.com>
2021-11-15 13:05:02 -0800

    to mend

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-// import Head from 'next/head' to bork
+import Head from 'next/head' to bork
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
