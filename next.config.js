72d41f4c6bca91ff835a9249f0baa6dc96005c5b
Cl3arglass <haltaffer@gmail.com>
2022-01-11 10:39:49 -0800

    source maps config

--- next.config.js
+++ next.config.js
@@ -13,13 +13,5 @@
 //   }
 
   module.exports = {
-    async rewrites() {
-      return [
-          {
-            source: '/dogs',
-            destination: 'https://montidogs-poc.vercel.app',
-            basePath: false,
-          },
-        ]
-    }
+    productionBrowserSourceMaps: true,
   }
