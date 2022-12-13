574e537f83b1656a34575ab18c21dd4f11e2407e
Cl3arglass <haltaffer@gmail.com>
2022-12-13 14:58:41 -0800

    vercel.json reqrite test

--- next.config.js
+++ next.config.js
@@ -1,20 +1,20 @@
-  module.exports = {
-    rewrites() {
-      return {
-          afterFiles: [
-              // if the host is `app.acme.com`,
-              // this rewrite will be applied
-              {
-                  source: '/:path*',
-                  has: [
-                      {
-                          type: 'host',
-                          value: 'about.holdenaltaffer.com',
-                      },
-                  ],
-                  destination: '/posts/:path*',
-              },
-          ]
-      }
-  }
-  }
+//   module.exports = {
+//     rewrites() {
+//       return {
+//           afterFiles: [
+//               // if the host is `app.acme.com`,
+//               // this rewrite will be applied
+//               {
+//                   source: '/:path*',
+//                   has: [
+//                       {
+//                           type: 'host',
+//                           value: 'about.holdenaltaffer.com',
+//                       },
+//                   ],
+//                   destination: '/posts/:path*',
+//               },
+//           ]
+//       }
+//   }
+//   }
