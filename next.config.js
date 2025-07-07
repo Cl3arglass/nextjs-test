25dc8a79f1d8be05ce2630551a6d4dd0cb219b94
Cl3arglass <haltaffer@gmail.com>
2025-07-07 14:45:57 -0700

    expermimental.cpu

--- next.config.js
+++ next.config.js
@@ -1,20 +1,5 @@
-//   module.exports = {
-//     rewrites() {
-//       return {
-//           afterFiles: [
-//               // if the host is `app.acme.com`,
-//               // this rewrite will be applied
-//               {
-//                   source: '/:path*',
-//                   has: [
-//                       {
-//                           type: 'host',
-//                           value: 'about.holdenaltaffer.com',
-//                       },
-//                   ],
-//                   destination: '/posts/:path*',
-//               },
-//           ]
-//       }
-//   }
-//   }
+  module.exports = {
+  experimental: {
+    cpus: 16
+  }
+}
